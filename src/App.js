c5ac87ceb5caf9384a595aa62671e48d6dab8233
stevenzhu <zhutaiyang@ruqimobility.com>
2019-09-25 15:16:49 +0800

    first

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/img/logo.svg';
+import './assets/css/App.css';
+
+import './assets/css/common.styl'
+
+import {connect} from 'react-redux'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="GreenH">
+          Edit <code>src/App.js</code> and save7777777777777777 765364536456456to reload.
         </p>
         <a
           className="App-link"
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import PropTypes from 'prop-types'
+import { createStore } from 'redux'
+import { Provider, connect } from 'react-redux'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
